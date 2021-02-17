c7fa694760cda661ce2dbd1a239831d959b086a4
Sungdong9Kim <sungdong9.kim@gmail.com>
2021-02-18 01:02:35 +0900

    feat : add custom dialog for inputting PIN number

---
+++ app/src/main/java/com/example/customdialog/CustomDialog2.java
@@ -0,0 +1,60 @@
+package com.example.customdialog;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.text.Editable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.view.ActionMode;
+import androidx.appcompat.widget.AppCompatEditText;
+
+import com.chaos.view.PinView;
+
+public class CustomDialog2 extends AppCompatEditText{
+
+    private Context context;
+
+    public CustomDialog2(Context context) {
+        super(context);
+        this.context = context;
+    }
+
+    public void callFunction(final TextView main_label) {
+        final Dialog dlg = new Dialog(context);
+
+        dlg.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        dlg.setContentView(R.layout.custom_dialog2);
+
+        dlg.show();
+
+        final EditText message = (EditText) dlg.findViewById(R.id.mesgase);
+        final Button okButton = (Button) dlg.findViewById(R.id.okButton);
+        final Button cancelButton = (Button) dlg.findViewById(R.id.cancelButton);
+
+        //((PinView) findViewById(R.id.pinView)).setAnimationEnable(true);
+        //((PinView) findViewById(R.id.pinView)).setPasswordHidden(true);
+
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dlg.dismiss();
+            }
+        });
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dlg.dismiss();
+            }
+        });
+    }
+}
+
--- app/src/main/java/com/example/customdialog/MainActivity.java
+++ app/src/main/java/com/example/customdialog/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.customdialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.chaos.view.PinView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +18,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button button = (Button) findViewById(R.id.button);
+        Button button2 = (Button) findViewById(R.id.button2);
+
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.custom_dialog2, null);
+
+        ((PinView) dialogView.findViewById(R.id.pinView)).setAnimationEnable(true);
+        ((PinView) dialogView.findViewById(R.id.pinView)).setPasswordHidden(true);
 
         final TextView main_label = (TextView) findViewById(R.id.main_label) ;
         button.setOnClickListener(new View.OnClickListener() {
@@ -24,5 +34,18 @@ public class MainActivity extends AppCompatActivity {
                 customDialog.callFunction(main_label);
             }
         });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CustomDialog2 customDialog = new CustomDialog2(MainActivity.this);
+                customDialog.callFunction(main_label);
+
+
+            }
+        });
+
+
     }
 }
+
